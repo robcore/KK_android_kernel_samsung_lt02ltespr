9661f128b2b03e0b2699af2f58ffc25210dda0a3
Jalen Battle-Bryant <flashalot.dev@gmail.com>
2014-11-11 23:43:19 -0600

    Initial commit for Samsung Galaxy Tab 3 7.0" Sprint
    Signed-off-by: Jalen Battle-Bryant <flashalot.dev@gmail.com>

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Nov 11 20:28:29 CST 2014"
+#define LINUX_COMPILE_BY "flashalot"
+#define LINUX_COMPILE_HOST "HotBox-1337"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
